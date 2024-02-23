91bf017f6bedf80bfe25b91fbde7e3a8ce17f0ff
Alex Hoffer <ahoffer@plaid.com>
2024-02-23 10:56:46 -0800

    upgrade to ngrok agent 3

--- server/routes/linkTokens.js
+++ server/routes/linkTokens.js
@@ -38,7 +38,7 @@ router.post(
       }
       const response = await fetch('http://ngrok:4040/api/tunnels');
       const { tunnels } = await response.json();
-      const httpTunnel = tunnels.find(t => t.proto === 'http');
+      const httpsTunnel = tunnels.find(t => t.proto === 'https');
       const linkTokenParams = {
         user: {
           // This should correspond to a unique id for the current user.
@@ -48,7 +48,7 @@ router.post(
         products,
         country_codes: ['US'],
         language: 'en',
-        webhook: httpTunnel.public_url + '/services/webhook',
+        webhook: httpsTunnel.public_url + '/services/webhook',
         access_token: accessToken,
       };
 
